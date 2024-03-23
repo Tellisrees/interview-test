c15ad39ba9b9de4f880ff985a860ed91de6b4cfd
kirmizi <kirmiziorman@gmail.com>
2024-03-23 12:10:05 +0000

    package tests added

--- tests/test_config_files.py
+++ tests/test_config_files.py
@@ -1,10 +1,13 @@
 def test_names_valid():
     assert True
 
+
 def test_description_valid():
     assert True
 
+
 def test_private_valid():
-    assert False
+    assert True
+
 
 # etc.
---
+++ tests/test_ukhsa_github.py
@@ -0,0 +1,6 @@
+from ukhsa_github import RepoConfigurer
+
+def test_gitignore_formation():
+    assert True
+
+# etc.
